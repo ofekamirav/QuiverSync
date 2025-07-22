f4e359d522710c57a5346256d854a68a6fed7b11
Ofek Amirav <128730112+ofekamirav@users.noreply.github.com>
2025-07-22 17:07:57 +0300

    Delete shared/src/commonMain/kotlin/org/example/quiversync/domain/usecase/user/GetUserByIDUseCase.kt

--- shared/src/commonMain/kotlin/org/example/quiversync/domain/usecase/user/GetUserByIDUseCase.kt
+++
@@ -1,9 +0,0 @@
-package org.example.quiversync.domain.usecase.user
-
-import org.example.quiversync.domain.repository.UserRepository
-
-class GetUserByIDUseCase(
-    private val userRepository: UserRepository
-) {
-    suspend operator fun invoke(userId: String) = userRepository.getUserById(userId)
-}
