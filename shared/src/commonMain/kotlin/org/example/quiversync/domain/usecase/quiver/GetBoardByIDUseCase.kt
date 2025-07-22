6a414148ce3933ee60972ef3e7ba70046e0f7958
Ofek Amirav <128730112+ofekamirav@users.noreply.github.com>
2025-07-22 17:05:01 +0300

    Delete shared/src/commonMain/kotlin/org/example/quiversync/domain/usecase/quiver/GetBoardByIDUseCase.kt

--- shared/src/commonMain/kotlin/org/example/quiversync/domain/usecase/quiver/GetBoardByIDUseCase.kt
+++
@@ -1,9 +0,0 @@
-package org.example.quiversync.domain.usecase.quiver
-
-import org.example.quiversync.domain.repository.QuiverRepository
-
-class GetBoardByIDUseCase (
-    private val quiverRepository: QuiverRepository
-){
-    suspend operator fun invoke(boardId: String) = quiverRepository.getBoardById(boardId)
-}
