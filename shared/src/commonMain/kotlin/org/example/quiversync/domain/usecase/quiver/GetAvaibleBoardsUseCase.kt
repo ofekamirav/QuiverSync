62a5c507db0a4dbf59c327df01c81cff8062299c
Ofek Amirav <128730112+ofekamirav@users.noreply.github.com>
2025-07-22 17:01:16 +0300

    Delete shared/src/commonMain/kotlin/org/example/quiversync/domain/usecase/quiver/GetAvaibleBoardsUseCase.kt

--- shared/src/commonMain/kotlin/org/example/quiversync/domain/usecase/quiver/GetAvaibleBoardsUseCase.kt
+++
@@ -1,9 +0,0 @@
-package org.example.quiversync.domain.usecase.quiver
-
-import org.example.quiversync.domain.repository.QuiverRepository
-
-class GetAvaibleBoardsUseCase(
-    private val quiverRepository: QuiverRepository
-) {
-    suspend operator fun invoke(userId: String) = quiverRepository.getAvailableSurfboards(userId)
-}
