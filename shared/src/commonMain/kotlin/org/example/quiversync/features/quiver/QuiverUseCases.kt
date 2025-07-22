bac0c3ce94fef9d3fbe174ed6ce79a686420ef98
Ofek Amirav <128730112+ofekamirav@users.noreply.github.com>
2025-07-22 17:48:12 +0300

    Update QuiverUseCases.kt

--- shared/src/commonMain/kotlin/org/example/quiversync/features/quiver/QuiverUseCases.kt
+++ shared/src/commonMain/kotlin/org/example/quiversync/features/quiver/QuiverUseCases.kt
@@ -2,12 +2,9 @@ package org.example.quiversync.features.quiver
 
 import org.example.quiversync.domain.usecase.quiver.AddBoardUseCase
 import org.example.quiversync.domain.usecase.quiver.DeleteSurfboardUseCase
-import org.example.quiversync.domain.usecase.quiver.GetBoardByIDUseCase
 import org.example.quiversync.domain.usecase.quiver.GetMyQuiverUseCase
 import org.example.quiversync.domain.usecase.quiver.PublishSurfboardToRentalUseCase
 import org.example.quiversync.domain.usecase.quiver.UnpublishSurfboardFromRentalUseCase
-import org.example.quiversync.domain.usecase.rentals.exploreRentals.AddRentalOfferUseCase
-import org.example.quiversync.domain.usecase.user.GetUserProfileUseCase
 
 data class QuiverUseCases(
     val getMyQuiverUseCase: GetMyQuiverUseCase,
@@ -15,12 +12,4 @@ data class QuiverUseCases(
     val addSurfboardUseCase: AddBoardUseCase,
     val publishSurfboardToRentalUseCase: PublishSurfboardToRentalUseCase,
     val unpublishForRentalUseCase: UnpublishSurfboardFromRentalUseCase,
-    val getBoardByIDUseCase: GetBoardByIDUseCase,
-
-    // Rental use cases
-    val addRentalOfferUseCase: AddRentalOfferUseCase,
-
-    //user useCases
-
-    val getUserProfileUseCase: GetUserProfileUseCase
 )
