bccbf8527eb0bc12be0b7feec76e25b9c63c28e2
Ofek Amirav <128730112+ofekamirav@users.noreply.github.com>
2025-07-22 17:06:56 +0300

    Update ExploreState.kt

--- shared/src/commonMain/kotlin/org/example/quiversync/features/exploreRentals/ExploreState.kt
+++ shared/src/commonMain/kotlin/org/example/quiversync/features/exploreRentals/ExploreState.kt
@@ -1,10 +1,10 @@
-package org.example.quiversync.features.exploreRentals
+package org.example.quiversync.features.rentals.explore
 
-import org.example.quiversync.domain.model.RentalOffer
+import org.example.quiversync.domain.model.BoardForRent
 import org.example.quiversync.domain.model.Surfboard
 
 sealed class ExploreState {
-    object Loading : ExploreState()
-    data class Loaded(val explorePageData: List<RentalOffer>) : ExploreState()
+    object Loading : ExploreState ()
+    data class Loaded(val communityBoards: List<BoardForRent>) : ExploreState()
     data class Error(val message: String) : ExploreState()
 }
